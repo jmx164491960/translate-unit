5f6c323115b8120ed28164e88ca584479b7b1efc
jiangminxi <jmx164491960@163.com>
2019-03-22 11:09:05 +0800

    1

---
+++ remove-repeat/index.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+function main(path, proPath) {
+  const code = fs.readFileSync(path, 'utf-8');
+  console.log('code:', code);
+  const data = JSON.parse(code);
+
+  let valueArr = [];
+  let jsonArr = [];
+  data.forEach((item) => {
+    const json = JSON.stringify(item);
+    // 没有重复的
+    if (jsonArr.indexOf(json) < 0) {
+      jsonArr.push(json);
+      valueArr.push(item);
+    }
+  });
+
+  fs.writeFile(proPath, JSON.stringify(valueArr, null, valueArr.length), (err) => {
+    if (!err) {
+      console.log(prodPath + '写入成功!');
+    }
+  })
+}
+main(path.resolve(__dirname, 'input.json'), path.resolve(__dirname, 'output.json'));
